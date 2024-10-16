e2edb9f2b9a7287ea97a2ae4c48238d69fae2916
Greg Fields <fieldsdev06@gmail.com>
2024-10-17 00:32:10 +0300

    [Feature] binary insertion sort ready

---
+++ sorting/include/binary_insertion_sort.h
@@ -0,0 +1,3 @@
+#include "../src/binary_insertion_sort.cpp"
+
+template<typename T> void binary_insertion_sort(T& arr);
---
+++ sorting/src/binary_insertion_sort.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <iterator>
+#include <array>
+
+using namespace std;
+
+template<typename T> bool isSorted(T& arr) {
+    int arr_size = arr.size();
+    for (int i = 0; i < arr_size; i++) {
+        if (arr[i] > arr[i+1]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+template<typename T> void binary(T& arr, int left, int right, int i) {
+    typename T::value_type temp;
+    int arr_size = arr.size();
+    int mid = (left+right)/2;
+
+    if (left >= right || arr[i] == arr[mid]) {
+        if (arr[i] > arr[left]) {
+            left++;
+        }
+        for (int j = i; left<j; j--) {
+            temp = arr[j-1];
+            arr[j-1] = arr[j];
+            arr[j] = temp;
+        }
+        
+    } else if (arr[i] < arr[mid]) {
+        binary(arr, left, mid-1, i);
+    } else {
+        binary(arr, mid+1, right, i);
+    }
+}
+
+
+template<typename T> void binary_insertion_sort(T& arr) {
+	typename T::value_type temp;
+    int arr_size = arr.size();
+    int currentI = 1;
+    
+    while(!isSorted(arr) && currentI < arr_size) {
+        binary(arr, 0, currentI-1, currentI);
+        currentI++;
+    }
+}
+
+// int main() {
+
+//     array<int, 5> arr = {8, 6, 1, 5, 3};
+//     binary_insertion_sort(arr);
+
+//     for (int i = 0; i<5; i++) {
+//         cout << arr[i] << ", ";
+//     }
+
+//     return 0;
+// }
